3d5160fb0f6f8864933d13657e9cc340e08cc695
Omar Hernandez <Omar.Hernandez@acoustic.co>
2024-04-22 16:18:38 -0700

    Update Package.swift
    CA-62924: Fix checksum and zip version.

--- Package.swift
+++ Package.swift
@@ -35,6 +35,6 @@ let package = Package(
         .binaryTarget(
             name: "AcousticMobilePush",
             url: "https://github.com/go-acoustic/Acoustic-Mobile-Push-iOS/releases/download/3.9.19/AcousticMobilePush_XCFramework_Release.zip",
-            checksum: "61b9f238f2743125186a70769dc6efecd1cca4ebc2a3621c38462a190141da4e"),
+            checksum: "e2b13add2a10871af8ae0503d2a381ae847241dc6eb70e6e9971980fa710c9ec"),
     ]
 )
